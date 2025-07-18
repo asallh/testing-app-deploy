a89fa2f6e9a66fd506ca7cf68447b0db7a06e9bf
asallh <ajaypalsallh@gmail.com>
2025-07-18 10:20:54 -0600

    updating the app.py

--- app_config.py
+++ app_config.py
@@ -5,7 +5,7 @@ import psycopg2
 class MissingEnvironmentVariables(EnvironmentError):
     def __init__(self, env_var_name):
         self.message = f"Required environment variable {env_var_name} not set"
-        super.__init__(self.message)
+        super().__init__(self.message)
 
 class Constants:
     ENV = st.secrets.get("ENV", os.environ.get("ENV", "LOCAL")).upper()
@@ -25,22 +25,11 @@ class Constants:
         if value is None and required:
             raise MissingEnvironmentVariables(f"{key} not found in secrets for '{cls.ENV}'")
         return value
-        
-    @classmethod
-    def get_secrets(cls, key: str, required: bool = True):
-        env_secrets = st.secrets.get(cls.ENV, {})
 
-        if not env_secrets:
-            raise MissingEnvironmentVariables(f"Secrets for environment '{cls.ENV}' not found")
-
-        value = env_secrets.get(key)
-        if value is None and required:
-            raise MissingEnvironmentVariables(f"{key} not found in secrets for '{cls.ENV}'")
-        return value
-    
     @classmethod
     def get_database_credentials(cls):
-        if cls.ENV != "DEV" or cls.ENV != "PROD":
+        # Use secrets.toml for LOCAL, environment variable for DEV/PROD (Databricks)
+        if cls.ENV == "LOCAL":
             return {
                 "host": cls.get_secrets("HOST"),
                 "dbname": cls.get_secrets("DBNAME"),
@@ -48,11 +37,11 @@ class Constants:
                 "password": cls.get_secrets("TOKEN")
             }
         else:
-            conn_str = os.environ["database"]
-            if conn_str is None:
+            conn_str = os.environ.get("database")
+            if not conn_str:
                 raise MissingEnvironmentVariables("Environment variable 'database' not found (set via resource key)")
             return {"connection_string": conn_str}
-        
+
     @classmethod
     def get_database_connection(cls):
         creds = cls.get_database_credentials()
