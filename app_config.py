b6bc36b2c2b3468c1c03565c05611f066fb6f3b1
asallh <ajaypalsallh@gmail.com>
2025-07-23 22:38:50 -0600

    resort to env

--- app_config.py
+++ app_config.py
@@ -13,10 +13,22 @@ class Constants:
 
     @classmethod
     def get_database_credentials(cls):
-        credentials =  {
-            "host": st.secrets["HOST"],
-            "dbname": st.secrets["DBNAME"],
-            "user": st.secrets["USER"],
-            "password": st.secrets["TOKEN"],
-        }
+        try:
+            credentials = {
+                "host": st.secrets["HOST"],
+                "dbname": st.secrets["DBNAME"],
+                "user": st.secrets["USER"],
+                "password": st.secrets["TOKEN"],
+            }
+        except (AttributeError, KeyError):
+            credentials = {
+                "host": os.environ.get("HOST"),
+                "dbname": os.environ.get("DBNAME"),
+                "user": os.environ.get("USER"),
+                "password": os.environ.get("TOKEN"),
+            }
+            # Optionally, raise if any are missing
+            for key, value in credentials.items():
+                if value is None:
+                    raise MissingEnvironmentVariables(key)
         return psycopg2.connect(**credentials)
