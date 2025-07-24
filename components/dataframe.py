a6f5a96d59dc0fe8f9d741c25fa4e178b661ad69
asallh <ajaypalsallh@gmail.com>
2025-07-24 15:42:13 -0600

    debuggong

--- components/dataframe.py
+++ components/dataframe.py
@@ -1,17 +1,27 @@
+import os
 import psycopg2
 import pandas as pd
 import streamlit as st
 
 from app_config import Constants
 
+
 class DataViewer:
 
     def __init__(self):
-        self.conn = Constants.get_database_credentials()
-        
+        # self.conn = Constants.get_database_credentials()
+        self.conn = {
+            "dbname": os.getenv("PGDATABASE"),
+            "user": os.getenv("PGUSER"),
+            "host": os.getenv("PGHOST"),
+            "port": os.getenv("PGPORT"),
+            "sslmode": os.getenv("PGSSLMODE"),
+            "application_name": os.getenv("PGAPPNAME"),
+        }
+
     def fetch_data(self, query):
         return pd.read_sql_query(query, self.conn)
-    
+
     def view_data(self):
         query = "SELECT * FROM my_dogs.dogs"
         df = self.fetch_data(query)
