d3638b932279ea071ea29b3c2931d60835d7c07c
asallh <ajaypalsallh@gmail.com>
2025-07-23 22:46:34 -0600

    reverting back to original

--- app_config.py
+++ app_config.py
@@ -9,37 +9,14 @@ class MissingEnvironmentVariables(EnvironmentError):
         super().__init__(self.message)
 
 
-def _get_secrets_credentials():
-    try:
-        return {
+class Constants:
+
+    @classmethod
+    def get_database_credentials(cls):
+        credentials =  {
             "host": st.secrets["HOST"],
             "dbname": st.secrets["DBNAME"],
             "user": st.secrets["USER"],
             "password": st.secrets["TOKEN"],
         }
-    except (AttributeError, KeyError, FileNotFoundError):
-        return None
-
-
-def _get_env_credentials():
-    return {
-        "host": os.environ.get("HOST"),
-        "dbname": os.environ.get("DBNAME"),
-        "user": os.environ.get("USER"),
-        "password": os.environ.get("TOKEN"),
-    }
-
-
-def _validate_credentials(credentials):
-    for key, value in credentials.items():
-        if value is None:
-            raise MissingEnvironmentVariables(key)
-
-
-class Constants:
-
-    @classmethod
-    def get_database_credentials(cls):
-        credentials = _get_secrets_credentials() or _get_env_credentials()
-        _validate_credentials(credentials)
         return psycopg2.connect(**credentials)
