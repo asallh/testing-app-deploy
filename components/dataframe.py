4e48ba7255eab6880e5bc29d9626382e04177aff
asallh <ajaypalsallh@gmail.com>
2025-07-24 20:12:49 -0600

    small update?

--- components/dataframe.py
+++ components/dataframe.py
@@ -15,6 +15,6 @@ class DataViewer:
         return pd.read_sql_query(query, self.conn)
 
     def view_data(self):
-        query = "SELECT * FROM my_dogs.dogs"
+        query = "SELECT * FROM dogs"
         df = self.fetch_data(query)
         st.dataframe(df)
